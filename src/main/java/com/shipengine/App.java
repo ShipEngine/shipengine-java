fefa18ee661dc35a57853bca586f595bfccde460
Pierce Harmon <pierceh.dev@gmail.com>
2021-08-05 10:27:22 -0500

    scaffold project

---
+++ src/main/java/com/shipengine/App.java
@@ -0,0 +1,13 @@
+package com.shipengine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
