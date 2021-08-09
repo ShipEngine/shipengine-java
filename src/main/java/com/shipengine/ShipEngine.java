4b2bf14732578ab04115e3de4633fda4372f1f59
Pierce Harmon <pierceh.dev@gmail.com>
2021-08-09 14:06:10 -0500

    W.I.P.

--- src/main/java/com/shipengine/ShipEngine.java
+++ src/main/java/com/shipengine/ShipEngine.java
@@ -2,44 +2,42 @@ package com.shipengine;
 
 import com.shipengine.Config;
 
-public class ShipEngine
-{
-    Config config;
+public class ShipEngine {
+  Config config;
 
-    public ShipEngine(String apiKey)
-    {
-        config = new Config(apiKey);
-    }
+  public ShipEngine(String apiKey) {
+    config = new Config(apiKey);
+  }
 
-    public String validateAddresses() {
-      return config.baseURL();
-    }
+  public String validateAddresses() {
+    return config.baseURL();
+  }
 
-    public String listCarriers() {
-      return config.baseURL();
-    }
+  public String listCarriers() {
+    return config.baseURL();
+  }
 
-    public String trackUsingCarrierCodeAndTrackingNumber() {
-      return config.baseURL();
-    }
+  public String trackUsingCarrierCodeAndTrackingNumber() {
+    return config.baseURL();
+  }
 
-    public String trackUsingLabelId() {
-      return config.baseURL();
-    }
+  public String trackUsingLabelId() {
+    return config.baseURL();
+  }
 
-    public String createLabelFromShipmentDetails() {
-      return config.baseURL();
-    }
+  public String createLabelFromShipmentDetails() {
+    return config.baseURL();
+  }
 
-    public String createLabelFromRate() {
-      return config.baseURL();
-    }
+  public String createLabelFromRate() {
+    return config.baseURL();
+  }
 
-    public String voidLabelWithLabelId() {
-      return config.baseURL();
-    }
+  public String voidLabelWithLabelId() {
+    return config.baseURL();
+  }
 
-    public String getRatesWithShipmentDetails() {
-      return config.baseURL();
-    }
+  public String getRatesWithShipmentDetails() {
+    return config.baseURL();
+  }
 }
