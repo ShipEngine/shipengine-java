c9773a32c07c4ce596edccc70bea9b725c52d8be
KaseyCantu <kpc.software.engineer@gmail.com>
2021-08-31 17:35:38 -0500

    Added docstrings

--- src/main/java/com/shipengine/ShipEngine.java
+++ src/main/java/com/shipengine/ShipEngine.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 public class ShipEngine {
+    private InternalClient client = new InternalClient();
     private Config config;
 
     public ShipEngine(String apiKey) {
@@ -20,10 +21,18 @@ public class ShipEngine {
         return config;
     }
 
-    public List validateAddresses(List<HashMap<String , String>> address) {
+    /**
+     * Address validation ensures accurate addresses and can lead to reduced shipping costs by preventing address
+     * correction surcharges. ShipEngine cross-references multiple databases to validate addresses and identify
+     * potential deliverability issues.
+     * See: https://shipengine.github.io/shipengine-openapi/#operation/validate_address
+     *
+     * @param address A list of HashMaps where each HashMap contains the address data to be validated.
+     * @return The response from ShipEngine API including the validated and normalized address.
+     */
+    public List validateAddresses(List<HashMap<String, String>> address) {
         Config mergedConfig = this.config.merge();
-        List<HashMap<String , String>> apiResponse = new ArrayList<>();
-        InternalClient client = new InternalClient();
+        List<HashMap<String, String>> apiResponse = new ArrayList<>();
         try {
             apiResponse = client.post(
                     "/v1/addresses/validate",
@@ -37,10 +46,20 @@ public class ShipEngine {
         return apiResponse;
     }
 
-    public List validateAddresses(List<HashMap<String , String>> address, Map<String, Object> config) {
+    /**
+     * Address validation ensures accurate addresses and can lead to reduced shipping costs by preventing address
+     * correction surcharges. ShipEngine cross-references multiple databases to validate addresses and identify
+     * potential deliverability issues.
+     * See: https://shipengine.github.io/shipengine-openapi/#operation/validate_address
+     *
+     * @param address A list of HashMaps where each HashMap contains the address data to be validated.
+     * @param config  Method level configuration to set new values for properties of the
+     *                global ShipEngineConfig object that will only affect the current request, not all requests.
+     * @return The response from ShipEngine API including the validated and normalized address.
+     */
+    public List validateAddresses(List<HashMap<String, String>> address, Map<String, Object> config) {
         Config mergedConfig = this.config.merge(config);
-        List<HashMap<String , String>> apiResponse = new ArrayList<>();
-        InternalClient client = new InternalClient();
+        List<HashMap<String, String>> apiResponse = new ArrayList<>();
         try {
             apiResponse = client.post(
                     "/v1/addresses/validate",
@@ -57,7 +76,6 @@ public class ShipEngine {
     public Map listCarriers() {
         Config mergedConfig = this.config.merge();
         Map apiResponse = new HashMap<>();
-        InternalClient client = new InternalClient();
         try {
             apiResponse = client.get(
                     "/v1/carriers",
@@ -73,7 +91,6 @@ public class ShipEngine {
     public Map listCarriers(Map<String, Object> config) {
         Config mergedConfig = this.config.merge(config);
         Map apiResponse = new HashMap<>();
-        InternalClient client = new InternalClient();
         try {
             apiResponse = client.get(
                     "/v1/carriers",
@@ -102,8 +119,51 @@ public class ShipEngine {
         return config.getBaseUrl();
     }
 
-    public String voidLabelWithLabelId() {
-        return config.getBaseUrl();
+    /**
+     * Void label with a Label ID.
+     * See: https://shipengine.github.io/shipengine-openapi/#operation/void_label
+     *
+     * @param labelId The label_id of the label you wish to void.
+     * @return The response from ShipEngine API confirming the label was successfully voided or
+     * unable to be voided.
+     */
+    public Map voidLabelWithLabelId(String labelId) {
+        Config mergedConfig = this.config.merge();
+        Map apiResponse = new HashMap<>();
+        try {
+            apiResponse = client.get(
+                    String.format("/v1/labels/%s/void", labelId),
+                    mergedConfig
+            );
+            return apiResponse;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return apiResponse;
+    }
+
+    /**
+     * Void label with a Label ID.
+     * See: https://shipengine.github.io/shipengine-openapi/#operation/void_label
+     *
+     * @param labelId The label_id of the label you wish to void.
+     * @param config  Method level configuration to set new values for properties of the
+     *                global ShipEngineConfig object that will only affect the current request, not all requests.
+     * @return The response from ShipEngine API confirming the label was successfully voided or unable to be voided.
+     */
+    public Map voidLabelWithLabelId(String labelId, Map<String, Object> config) {
+        Config mergedConfig = this.config.merge(config);
+        Map apiResponse = new HashMap<>();
+        try {
+            apiResponse = client.get(
+                    String.format("/v1/labels/%s/void", labelId),
+                    mergedConfig
+            );
+            return apiResponse;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return apiResponse;
     }
 
     public String getRatesWithShipmentDetails() {
